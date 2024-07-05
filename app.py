476db9c91d72e1f2fb71fb4bf540c20bfebfcf49
tymoneq <tymontumialis@gmail.com>
2024-07-05 07:36:30 +0200

    fafo

--- app.py
+++ app.py
@@ -10,6 +10,18 @@ stockData = pd.read_excel("stockData.xlsx",index_col="Unnamed: 0")
 
 st.title("📊 Porownywarka Danych Finansowych Firm 📊")
 
+img_col1,img_col2,img_col3 = st.columns(3)
+
+with img_col1:
+    st.image("pepe.png")
+
+with img_col2:
+    st.image("pepe1.jpeg")
+    
+with img_col3:
+    st.image("winnerscompound.png")
+    
+st.header("Kwartalna Dynamika:")
 
 col1, col2 = st.columns(2)
 
@@ -19,19 +31,25 @@ with col1:
                    "Zysk netto akcjonariuszy jednostki dominującej"]))
     
 with col2:
-    dynamics = st.number_input("Jaka ma byc dynamika zmian w %?",value=10.00,step = 0.5)
+    dynamics = float(st.number_input("Jaka ma byc dynamika zmian w %?",value=10.00,step = 0.5))
 
 df_reversed = stockData.iloc[::-1]
 diff = df_reversed.diff().iloc[::-1]
 
-
 #highlights the diffrence between two datas
 def highlightChange(s):
     color = 'black'
-    name = s.name
-    # st.write(name)
-    if s[options] > 1000000:
-        color  = "green"
+    name = str(s.name)
+    row_index = stockData.index.get_loc(name)
+    prev_row_index = row_index+1
+    try:
+        prev_row_data = stockData.iloc[prev_row_index]
+        change = prev_row_data[options] * (1 + (dynamics/100))
+        if s[options] >= change:
+            color  = "green"
+    except:
+        color='black'
+   
     return ['background-color: {}'.format(color)]* len(s)
 
 #highlight every cell in diffrence data frame
@@ -39,7 +57,9 @@ def highlightedChanges(val):
     color = 'green' if val > 0 else 'red'
     return f'background-color: {color}'
    
-
-st.dataframe(stockData.style.apply(highlightChange,axis=1).format(thousands='.'),width=1800)
-st.dataframe(diff.style.format(thousands='.').applymap(highlightedChanges,subset = list(stockData.columns)),width=1800)
+with st.container():
+    st.header("Dane Finansowe CDR")
+    st.dataframe(stockData.style.apply(highlightChange,axis=1).format(thousands='.'),width=1800)
+    st.header("Kwartalne Zmiany w Danych Finansowych CDR")
+    st.dataframe(diff.style.format(thousands='.',precision=2).applymap(highlightedChanges,subset = list(stockData.columns)),width=1800)
 
